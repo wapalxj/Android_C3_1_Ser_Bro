71a2e23f6172920a0e9cb2caf93a09d25b03b6bd
wapalxj <wapalxj@163.com>
2016-01-22 17:08:32 +0800

    16.01.22

---
+++ C3_1_Service_Broadcast/c3_8_messenger_service/src/androidTest/java/com/book/vero/c3_8_messenger_service/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.book.vero.c3_8_messenger_service;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
